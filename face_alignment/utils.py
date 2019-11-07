08213698666d6f5b7b52ea81791c7ae350d44b10
Jake <spacejake@gmail.com>
2019-11-07 11:50:54 +0900

    code for ppt

--- face_alignment/utils.py
+++ face_alignment/utils.py
@@ -214,6 +214,62 @@ def transform(point, transform, invert=False):
 #
 #     return new_point.int()
 
+def crop_only(image, center, scale, resolution=256.0, rotate=0):
+    """Center crops an image or set of heatmaps
+
+    Arguments:
+        image {numpy.array} -- an rgb image
+        center {numpy.array} -- the center of the object, usually the same as of the bounding box
+        scale {float} -- scale of the face
+
+    Keyword Arguments:
+        resolution {float} -- the size of the output cropped image (default: {256.0})
+
+    Returns:
+        [type] -- [description]
+    """  # Crop around the center point
+    """ Crops the image around the center. Input is expected to be an np.ndarray """
+    transul = getTransform(center, scale, resolution)
+    ul = transform([1, 1], transul, True).int()
+
+    transbr = getTransform(center, scale, resolution)
+    br = transform([resolution, resolution], transbr, True).int()
+    # ul = transform([1, 1], center, scale, resolution, True)
+    # br = transform([resolution, resolution], center, scale, resolution, True)
+
+    pad = math.ceil(torch.norm((ul - br).float()) / 2.0 - (br[0] - ul[0]) / 2.0)
+    if rotate is not 0:
+        ul -= pad
+        br += pad
+
+
+    # plt.imshow(image)
+    # plt.show()
+
+    if image.ndim > 2:
+        newDim = np.array([br[1] - ul[1], br[0] - ul[0],
+                           image.shape[2]], dtype=np.int32)
+        newImg = np.zeros(newDim, dtype=np.uint8)
+    else:
+        newDim = np.array([br[1] - ul[1], br[0] - ul[0]], dtype=np.int)
+        newImg = np.zeros(newDim, dtype=np.uint8)
+
+    # plt.imshow(newImg)
+    # plt.show()
+
+    ht = image.shape[0]
+    wd = image.shape[1]
+    newX = np.array(
+        [max(1, -ul[0] + 1), min(br[0], wd) - ul[0]], dtype=np.int32)
+    newY = np.array(
+        [max(1, -ul[1] + 1), min(br[1], ht) - ul[1]], dtype=np.int32)
+    oldX = np.array([max(1, ul[0] + 1), min(br[0], wd)], dtype=np.int32)
+    oldY = np.array([max(1, ul[1] + 1), min(br[1], ht)], dtype=np.int32)
+    newImg[newY[0] - 1:newY[1], newX[0] - 1:newX[1]
+           ] = image[oldY[0] - 1:oldY[1], oldX[0] - 1:oldX[1], :]
+
+    return newImg
+
 def crop(image, center, scale, resolution=256.0, rotate=0):
     """Center crops an image or set of heatmaps
 
